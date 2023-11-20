a89051c1f65d0c508071995fa28cde3460c2ae6e
Reddemjyoshna <62939217+Reddemjyoshna@users.noreply.github.com>
2023-11-20 20:24:41 +0530

    Add files via upload

---
+++ reverseArray.java
@@ -0,0 +1,15 @@
+package com.codes;
+
+public class reverseArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {10, 20, 30, 40, 50};
+
+	     int n=arr.length;
+	     for(int i=n-1; i>=0; i--)
+	       System.out.print(arr[i]+" "); 
+
+	}
+
+}
